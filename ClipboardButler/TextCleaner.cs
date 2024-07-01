d34c8b3af10d26e55d91cf42037a7122e162a899
nopara73 <adam.ficsor73@gmail.com>
2024-07-01 13:54:50 +0200

    add another heuristic

--- ClipboardButler/TextCleaner.cs
+++ ClipboardButler/TextCleaner.cs
@@ -8,11 +8,13 @@ namespace ClipboardButler
 {
     public static class TextCleaner
     {
-        public static bool TryClean(string dirty, out string clean) 
+        public static bool TryClean(string dirty, out string clean)
         {
             // Check if the URL starts with "https://youtu.be/"
-            string prefix = "https://youtu.be/";
-            if (dirty.StartsWith(prefix))
+            string youtuBePrefix = "https://youtu.be/";
+            string youtubePrefix = "https://www.youtube.com/watch?v=";
+
+            if (dirty.StartsWith(youtuBePrefix))
             {
                 // Find the position of the '?' character
                 int questionMarkIndex = dirty.IndexOf('?');
@@ -22,8 +24,26 @@ namespace ClipboardButler
                     clean = dirty.Substring(0, questionMarkIndex);
                     return true;
                 }
+                // If no query parameters are found, return the original URL
+                clean = dirty;
+                return true;
             }
-            // Return the original URL if no query parameters were found
+            else if (dirty.StartsWith(youtubePrefix))
+            {
+                // Find the position of the '&' character
+                int ampersandIndex = dirty.IndexOf('&');
+                if (ampersandIndex > -1)
+                {
+                    // Return the URL without the query parameters after the video ID
+                    clean = dirty.Substring(0, ampersandIndex);
+                    return true;
+                }
+                // If no query parameters are found, return the original URL
+                clean = dirty;
+                return true;
+            }
+
+            // Return the original URL if it doesn't match the known patterns
             clean = dirty;
             return false;
         }
