b0e08b6db3e6c95223e8e22042cb9acd951bda4f
nopara73 <adam.ficsor73@gmail.com>
2024-07-01 13:48:11 +0200

    extract things to traymanager

--- ClipboardButler/Program.cs
+++ ClipboardButler/Program.cs
@@ -12,71 +12,8 @@ namespace ClipboardButler
         [DllImport("user32.dll")]
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        [DllImport("user32.dll")]
-        private static extern IntPtr LoadIcon(IntPtr hInstance, IntPtr lpIconName);
-
-        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
-        private static extern bool Shell_NotifyIcon(uint dwMessage, [In] ref NOTIFYICONDATA lpData);
-
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        private delegate IntPtr WindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        private struct WNDCLASSEX
-        {
-            public uint cbSize;
-            public uint style;
-            public IntPtr lpfnWndProc;
-            public int cbClsExtra;
-            public int cbWndExtra;
-            public IntPtr hInstance;
-            public IntPtr hIcon;
-            public IntPtr hCursor;
-            public IntPtr hbrBackground;
-            public string lpszMenuName;
-            public string lpszClassName;
-            public IntPtr hIconSm;
-        }
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        private struct NOTIFYICONDATA
-        {
-            public uint cbSize;
-            public IntPtr hWnd;
-            public uint uID;
-            public uint uFlags;
-            public uint uCallbackMessage;
-            public IntPtr hIcon;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
-            public string szTip;
-            public uint dwState;
-            public uint dwStateMask;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
-            public string szInfo;
-            public uint uTimeoutOrVersion;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
-            public string szInfoTitle;
-            public uint dwInfoFlags;
-            public Guid guidItem;
-            public IntPtr hBalloonIcon;
-        }
-
         private const int SW_HIDE = 0;
         private const int SW_SHOW = 5;
-        private const uint NIM_ADD = 0x00000000;
-        private const uint NIM_MODIFY = 0x00000001;
-        private const uint NIM_DELETE = 0x00000002;
-        private const uint NIF_MESSAGE = 0x00000001;
-        private const uint NIF_ICON = 0x00000002;
-        private const uint NIF_TIP = 0x00000004;
-        private const int WM_USER = 0x0400;
-        private const int WM_LBUTTONUP = 0x0202;
-
-        private const string CLASS_NAME = "ClipboardButlerWindowClass";
-
-        private static WindowProc wndProcDelegate = new WindowProc(WndProc);
 
         static async Task Main(string[] args)
         {
@@ -86,8 +23,8 @@ namespace ClipboardButler
             ShowWindow(handle, SW_HIDE);
 
             // Add to system tray
-            IntPtr hWnd = CreateMessageWindow();
-            AddToSystemTray(hWnd);
+            IntPtr hWnd = TrayManager.CreateMessageWindow();
+            TrayManager.AddToSystemTray(hWnd);
 
             while (true)
             {
@@ -108,90 +45,5 @@ namespace ClipboardButler
                 }
             }
         }
-
-        private static IntPtr CreateMessageWindow()
-        {
-            WNDCLASSEX wndClassEx = new WNDCLASSEX
-            {
-                cbSize = (uint)Marshal.SizeOf(typeof(WNDCLASSEX)),
-                style = 0,
-                lpfnWndProc = Marshal.GetFunctionPointerForDelegate(wndProcDelegate),
-                cbClsExtra = 0,
-                cbWndExtra = 0,
-                hInstance = GetModuleHandle(null),
-                hIcon = IntPtr.Zero,
-                hCursor = IntPtr.Zero,
-                hbrBackground = IntPtr.Zero,
-                lpszMenuName = null,
-                lpszClassName = CLASS_NAME,
-                hIconSm = IntPtr.Zero
-            };
-
-            RegisterClassEx(ref wndClassEx);
-
-            return CreateWindowEx(
-                0,
-                CLASS_NAME,
-                "Clipboard Butler",
-                0,
-                0, 0, 0, 0,
-                IntPtr.Zero, IntPtr.Zero, GetModuleHandle(null), IntPtr.Zero);
-        }
-
-        private static IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
-        {
-            switch (msg)
-            {
-                case WM_USER + 1:
-                    if (lParam.ToInt32() == WM_LBUTTONUP)
-                    {
-                        // Handle left button click
-                    }
-                    break;
-                default:
-                    return DefWindowProc(hWnd, msg, wParam, lParam);
-            }
-            return IntPtr.Zero;
-        }
-
-        private static void AddToSystemTray(IntPtr hWnd)
-        {
-            var hInstance = GetModuleHandle(null);
-            var hIcon = LoadIcon(IntPtr.Zero, (IntPtr)0x7F00); // Default application icon
-
-            var notifyIconData = new NOTIFYICONDATA
-            {
-                cbSize = (uint)Marshal.SizeOf(typeof(NOTIFYICONDATA)),
-                hWnd = hWnd,
-                uID = 1,
-                uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP,
-                uCallbackMessage = WM_USER + 1,
-                hIcon = hIcon,
-                szTip = "Clipboard Butler"
-            };
-
-            Shell_NotifyIcon(NIM_ADD, ref notifyIconData);
-        }
-
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern ushort RegisterClassEx([In] ref WNDCLASSEX lpwcx);
-
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern IntPtr CreateWindowEx(
-            int dwExStyle,
-            string lpClassName,
-            string lpWindowName,
-            int dwStyle,
-            int x,
-            int y,
-            int nWidth,
-            int nHeight,
-            IntPtr hWndParent,
-            IntPtr hMenu,
-            IntPtr hInstance,
-            IntPtr lpParam);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr DefWindowProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
     }
 }
---
+++ ClipboardButler/TrayManager.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ClipboardButler
+{
+    internal static class TrayManager
+    {
+        private delegate IntPtr WindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct WNDCLASSEX
+        {
+            public uint cbSize;
+            public uint style;
+            public IntPtr lpfnWndProc;
+            public int cbClsExtra;
+            public int cbWndExtra;
+            public IntPtr hInstance;
+            public IntPtr hIcon;
+            public IntPtr hCursor;
+            public IntPtr hbrBackground;
+            public string lpszMenuName;
+            public string lpszClassName;
+            public IntPtr hIconSm;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct NOTIFYICONDATA
+        {
+            public uint cbSize;
+            public IntPtr hWnd;
+            public uint uID;
+            public uint uFlags;
+            public uint uCallbackMessage;
+            public IntPtr hIcon;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 128)]
+            public string szTip;
+            public uint dwState;
+            public uint dwStateMask;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string szInfo;
+            public uint uTimeoutOrVersion;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 64)]
+            public string szInfoTitle;
+            public uint dwInfoFlags;
+            public Guid guidItem;
+            public IntPtr hBalloonIcon;
+        }
+
+        private const string CLASS_NAME = "ClipboardButlerWindowClass";
+        private static readonly WindowProc wndProcDelegate = new WindowProc(WndProc);
+
+        public static IntPtr CreateMessageWindow()
+        {
+            WNDCLASSEX wndClassEx = new WNDCLASSEX
+            {
+                cbSize = (uint)Marshal.SizeOf(typeof(WNDCLASSEX)),
+                style = 0,
+                lpfnWndProc = Marshal.GetFunctionPointerForDelegate(wndProcDelegate),
+                cbClsExtra = 0,
+                cbWndExtra = 0,
+                hInstance = GetModuleHandle(null),
+                hIcon = IntPtr.Zero,
+                hCursor = IntPtr.Zero,
+                hbrBackground = IntPtr.Zero,
+                lpszMenuName = null,
+                lpszClassName = CLASS_NAME,
+                hIconSm = IntPtr.Zero
+            };
+
+            RegisterClassEx(ref wndClassEx);
+
+            return CreateWindowEx(
+                0,
+                CLASS_NAME,
+                "Clipboard Butler",
+                0,
+                0, 0, 0, 0,
+                IntPtr.Zero, IntPtr.Zero, GetModuleHandle(null), IntPtr.Zero);
+        }
+
+        public static void AddToSystemTray(IntPtr hWnd)
+        {
+            var hInstance = GetModuleHandle(null);
+            var hIcon = LoadIcon(IntPtr.Zero, (IntPtr)0x7F00); // Default application icon
+
+            var notifyIconData = new NOTIFYICONDATA
+            {
+                cbSize = (uint)Marshal.SizeOf(typeof(NOTIFYICONDATA)),
+                hWnd = hWnd,
+                uID = 1,
+                uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP,
+                uCallbackMessage = WM_USER + 1,
+                hIcon = hIcon,
+                szTip = "Clipboard Butler"
+            };
+
+            Shell_NotifyIcon(NIM_ADD, ref notifyIconData);
+        }
+
+        private static IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            switch (msg)
+            {
+                case WM_USER + 1:
+                    if (lParam.ToInt32() == WM_LBUTTONUP)
+                    {
+                        // Handle left button click
+                    }
+                    break;
+                default:
+                    return DefWindowProc(hWnd, msg, wParam, lParam);
+            }
+            return IntPtr.Zero;
+        }
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern ushort RegisterClassEx([In] ref WNDCLASSEX lpwcx);
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern IntPtr CreateWindowEx(
+            int dwExStyle,
+            string lpClassName,
+            string lpWindowName,
+            int dwStyle,
+            int x,
+            int y,
+            int nWidth,
+            int nHeight,
+            IntPtr hWndParent,
+            IntPtr hMenu,
+            IntPtr hInstance,
+            IntPtr lpParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr DefWindowProc(IntPtr hWnd, uint uMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr LoadIcon(IntPtr hInstance, IntPtr lpIconName);
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        private static extern bool Shell_NotifyIcon(uint dwMessage, [In] ref NOTIFYICONDATA lpData);
+
+        private const int WM_USER = 0x0400;
+        private const int WM_LBUTTONUP = 0x0202;
+        private const uint NIF_MESSAGE = 0x00000001;
+        private const uint NIF_ICON = 0x00000002;
+        private const uint NIF_TIP = 0x00000004;
+        private const uint NIM_ADD = 0x00000000;
+        private const uint NIM_MODIFY = 0x00000001;
+        private const uint NIM_DELETE = 0x00000002;
+    }
+}
