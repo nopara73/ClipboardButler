9fc23f81910dc1c9390c6a6d94357a623f4a9591
nopara73 <adam.ficsor73@gmail.com>
2024-07-01 11:36:22 +0200

    Add project files.

---
+++ ClipboardButler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ClipboardButler
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+
+        static async Task Main(string[] args)
+        {
+            var handle = GetConsoleWindow();
+
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+
+            while (true)
+            {
+                await Task.Delay(50);
+            }
+        }
+    }
+}
